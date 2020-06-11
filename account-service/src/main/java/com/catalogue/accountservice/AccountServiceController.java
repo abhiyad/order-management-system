f629986f4311af6e3e6248baabceea5bc37bb0cc
abhiyad <abhishekcms611@gmail.com>
2020-06-11 18:22:44 +0530

    Added Update Order Control

--- account-service/src/main/java/com/catalogue/accountservice/AccountServiceController.java
+++ account-service/src/main/java/com/catalogue/accountservice/AccountServiceController.java
@@ -7,6 +7,7 @@ import com.catalogue.accountservice.domain.MyUser;
 import com.catalogue.accountservice.domain.UserOrders;
 import com.catalogue.accountservice.services.JwtUserDetailsService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -26,6 +27,7 @@ import java.util.*;
 public class AccountServiceController {
     private final ObjectMapper objectMapper = new ObjectMapper();
     private JwtUserDetailsService userService;
+
     @Autowired
     public void setUserDetailsService(JwtUserDetailsService userService){
         this.userService = userService;
@@ -33,57 +35,83 @@ public class AccountServiceController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @RequestMapping("/hello")
-    public String firstPage(HttpSession session) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = ((UserDetails)authentication.getPrincipal()).getUsername();
-        System.out.println(" =================" + username);
-        return "Hello World";
-    }
-
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ResponseEntity<?> saveUser(@RequestBody MyUser user) throws Exception {
         return ResponseEntity.ok(userService.save(user));
     }
-    @GetMapping("/search/{isbn}")
+    @RequestMapping("/search/{isbn}")
     public Book search(@PathVariable("isbn")String isbn){
         Book book = restTemplate.getForObject("http://InventoryService/inventory/search/" + isbn , Book.class);
         return book;
     }
-    @GetMapping("/orders")
+    @RequestMapping("/orders")
     public List<CatalogueOrder> getAllOrders(){
+        List<Long>orderID = getOrderIDListUtil();
+        List<CatalogueOrder>allOrders = getAllOrdersUtil(orderID);
+        return allOrders;
+    }
+    @RequestMapping(value="/create/{isbn}")
+    public CatalogueOrder createOrder(@PathVariable("isbn")String isbn) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        Map<String, Object> map = getObjectMap(isbn,"false");
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
+        ResponseEntity<CatalogueOrder> response = restTemplate.postForEntity(
+                "http://OrderService/orders/create", entity, CatalogueOrder.class);
+        UserOrders userOrders = restTemplate.getForObject(
+                "http://UserOrderService/orders/add/" + getUsername() + "/" + String.valueOf(response.getBody().getId()),UserOrders.class);
+        System.out.println(userOrders.toString());
+        return response.getBody();
+    }
+    @RequestMapping(value="/update/{id}")
+    public CatalogueOrder updateOrder(@PathVariable("id") String id, @RequestBody CatalogueOrder updatedOrder)throws NotFoundException {
+        List<Long>orderIDList = getOrderIDListUtil();
+        if ( orderIDList.contains(Long.valueOf(id)) ){
+            // proceed with update
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            Map<String, Object> map = getObjectMap(updatedOrder.getIsbn(),String.valueOf(updatedOrder.getSent()));
+            HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
+            ResponseEntity<CatalogueOrder> response = restTemplate.postForEntity(
+                    "http://OrderService/orders/update/" + id, entity, CatalogueOrder.class);
+        }
+        else{
+            throw new NotFoundException("This Order does not belong to the User");
+        }
+        return updatedOrder;
+    }
+
+    public String getUsername(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = ((UserDetails)authentication.getPrincipal()).getUsername();
+        return username;
+    }
+
+    public List<Long> getOrderIDListUtil(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = ((UserDetails)authentication.getPrincipal()).getUsername();
         UserOrders response = restTemplate.getForObject("http://UserOrderService/orders/" + username, UserOrders.class);
         List<Long> orderID = response.getOrderId();
+        return orderID;
+    }
+
+    public List<CatalogueOrder> getAllOrdersUtil(List<Long>orderIDList){
         List<CatalogueOrder>allOrders = new ArrayList<>();
-        for (Long order : orderID ) {
+        for (Long order : orderIDList ) {
             CatalogueOrder thisOrder = restTemplate.getForObject("http://OrderService/orders/" + order, CatalogueOrder.class);
             allOrders.add(thisOrder);
         }
         return allOrders;
     }
-    @PostMapping(value="/create/{isbn}")
-    public CatalogueOrder createOrder(@PathVariable("isbn")String isbn) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = ((UserDetails)authentication.getPrincipal()).getUsername();
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+    public Map<String, Object> getObjectMap(String isbn, String sent){
+        String username = getUsername();
         Map<String, Object> map = new HashMap<>();
         map.put("username",username);
         map.put("isbn", isbn);
-        map.put("sent","false");
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
-        ResponseEntity<CatalogueOrder> response = restTemplate.postForEntity(
-                "http://OrderService/orders/create", entity, CatalogueOrder.class);
-        UserOrders userOrders = restTemplate.getForObject(
-                "http://UserOrderService/orders/add/" + username + "/" + String.valueOf(response.getBody().getId()),UserOrders.class);
-        System.out.println(userOrders.toString());
-        return response.getBody();
+        map.put("sent",sent);
+        return map;
     }
-//    @GetMapping("/search")
-//    public String search(){
-//        return "SEARCH";
-//    }
+
 }
